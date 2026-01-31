2f9bf6179ff2863bade202c6eb2d1e6847efe2f5
Samath Lakshan Ravihara <lakshanravihara03@gmail.com>
2026-01-31 09:38:07 +0530

    update

--- tests/positiveCases.js
+++ tests/positiveCases.js
@@ -2,12 +2,66 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Positive Functional Tests – Singlish to Sinhala', () => {
 
-  test('POS_FUN_0001-Simple sentences ,Proper spacing ', async ({ page }) => {
+  test('POS_FUN_0001-Convert a simple greeting ', async ({ page }) => {
     await page.goto('https://www.swifttranslator.com/', { timeout: 60000 });
 
-    await page.fill('textarea', 'mama yanavaa');
+    await page.fill('textarea', 'oyaata kohomadha?');
     await page.waitForTimeout(4000);
 
-    await expect(page.locator('body')).toContainText('මම යනවා');
+    await expect(page.locator('body')).toContainText('ඔයාට කොහොමද?');
+  });
+});
+
+
+test.describe('Positive Functional Tests – Singlish to Sinhala', () => {
+
+  test('POS_FUN_0002- Simple present tence Statement ', async ({ page }) => {
+    await page.goto('https://www.swifttranslator.com/', { timeout: 60000 });
+
+    await page.fill('textarea', 'Mama Gedara Yanawa');
+    await page.waitForTimeout(4000);
+
+    await expect(page.locator('body')).toContainText('මම ගෙදර යනවා');
+  });
+});
+
+
+test.describe('Positive Functional Tests – Singlish to Sinhala', () => {
+
+  test('POS_FUN_0003- Request with polite phrasing ', async ({ page }) => {
+    await page.goto('https://www.swifttranslator.com/', { timeout: 60000 });
+
+    await page.fill('textarea', 'karuNaakara eeka dhenavadha?');
+    await page.waitForTimeout(4000);
+
+    await expect(page.locator('body')).toContainText('karuNaakara eeka dhenavadha?');
+  });
+});
+
+
+
+test.describe('Positive Functional Tests – Singlish to Sinhala', () => {
+
+  test('POS_FUN_0004- Negative form sentence ', async ({ page }) => {
+    await page.goto('https://www.swifttranslator.com/', { timeout: 60000 });
+
+    await page.fill('textarea', 'mama ehema karannee naehae');
+    await page.waitForTimeout(4000);
+
+    await expect(page.locator('body')).toContainText('මම එහෙම කරන්නේ නැහැ');
+  });
+});
+
+
+
+test.describe('Positive Functional Tests – Singlish to Sinhala', () => {
+
+  test('POS_FUN_0005- Compound sentence ', async ({ page }) => {
+    await page.goto('https://www.swifttranslator.com/', { timeout: 60000 });
+
+    await page.fill('textarea', 'api kaeema kanna yanavaa saha passe chithrapatayakuth balanavaa');
+    await page.waitForTimeout(4000);
+
+    await expect(page.locator('body')).toContainText('අපි කෑම කන්න යනවා සහ පස්සෙ චිත්‍රපටයකුත් බලනවා');
   });
 });
